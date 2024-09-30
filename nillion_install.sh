60f95f959e763c12a486d7500279ebd82703ad5f
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-09-30 15:55:08 +0300

    Update nillion_install.sh

--- nillion_install.sh
+++ nillion_install.sh
@@ -18,5 +18,5 @@ fi
 
 mkdir -p nillion/verifier
 sleep 30
-docker run -d --name nillion_run_docker -v ./nillion/verifier:/var/tmp nillion/verifier:v1.0.1 initialise
+docker run -v ./nillion/verifier:/var/tmp nillion/verifier:v1.0.1 initialise
 echo "Установка завершена!"
