b11ef85ae21c47a134b4b6921d72e2ad6bbd7bdd
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-09-30 13:58:23 +0300

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-cache madison docker-ce
+sudo apt-get install docker-ce=27.1.1~3-0~ubuntu-$(lsb_release -cs) docker-ce-cli=27.1.1~3-0~ubuntu-$(lsb_release -cs) containerd.io
+sudo systemctl enable docker
+docker --version
