0c9457edbcfde954d962ab797ddabe0e9b0dc3b9
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-10-02 14:18:30 +0300

    Update nillion_install.sh

--- nillion_install.sh
+++ nillion_install.sh
@@ -8,17 +8,8 @@ sudo apt-get install --only-upgrade -y docker-ce docker-ce-cli containerd.io
 sudo systemctl restart docker
 docker --version
 
-# Останавливаем предыдущий докер
-if docker stop recursing_chandrasekhar; then
-  echo "Предыдущий образ успешно остановлен"
-else
-  echo "Ошибка загрузки образа" >&2
-  exit 1
-fi
-docker rm recursing_chandrasekhar
-
 # Закидываем докер ниллион
-if docker pull nillion/verifier:; then
+if docker pull nillion/verifier:1.0.1; then
   echo "Образ успешно загружен"
 else
   echo "Ошибка загрузки образа" >&2
@@ -27,5 +18,5 @@ fi
 
 mkdir -p nillion/verifier
 sleep 30
-docker run -v ./nillion/verifier:/var/tmp nillion/verifier: initialise
+docker run -v ./nillion/verifier:/var/tmp nillion/verifier:1.0.1 initialise
 echo "Установка завершена!"
