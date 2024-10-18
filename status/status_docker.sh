60f056de189352723a13c3674a07ac8c87cfea5e
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-10-18 15:07:28 +0300

    Update status_docker.sh

--- status/status_docker.sh
+++ status/status_docker.sh
@@ -4,43 +4,65 @@
 REPORT_FILENAME="docker_report_$(date '+%Y-%m-%d_%H-%M-%S').txt"
 REPORT_FILE="./$REPORT_FILENAME"
 
-# Параметры: ожидаемые и нежелательные контейнеры
+# Параметры для Docker-контейнеров
 NECESSARY_CONTAINERS=("elixir" "ipfs_node" "orchestrator" "run_nillion" "shardeum-dashboard" "allora-worker" "source-02" "source-03" "source-01" "updater" "mongodb" "docker-watchtower-1" "bevm-node")
 UNNECESSARY_CONTAINERS=("subspace_docker-node-1" "subspace_docker-farmer-1" "kroma-validator" "kroma-node" "kroma-geth")
+
+# Параметры для файлов и папок
+NECESSARY_ITEMS=("0g_backup" "0g-chain" "0g-storage-client" "0g-storage-node" "3proxy-0.9.3" "all-in-one-v2" "allora-chain" "allora-huggingface-walkthrough" "allora-worker-x-reputer" "basic-coin-prediction-node" "bevm" "btop.tbz" "data" "elixir" "go" "heminetwork" "infernet-container-starter" "installer.sh" "install_zabbix.sh" "nillion" "sentry-node-cli-linux" "star_labs" "start.sh" "subspace_docker" "update.txt" "vmagent-prod" "vmalert-prod" "vmauth-prod" "vmbackup-prod" "vmctl-prod" "vmrestore-prod" "zabbix-release_6.4-1+ubuntu22.04_all.deb" ".0gchain" ".config" ".profile" ".docker")
+UNNECESSARY_ITEMS=("0gchain_snapshot.lz4" "0gchain_snapshot.lz4.aria2" "0.9.3.tar.gz" "0.9.3.tar.gz.1" "BeraMachine" "dusk_global_height.json" "gear" "gear_key" "masa-oracle-go-testnet" "my-cryptopunks-squid" "sentry-node-cli-linux.zip" "storage_0gchain_snapshot.lz4" "uniform-load-squid" "zabbix-release_6.4-1+ubuntu22.04_all.deb.1" "zabbix-release_6.4-1+ubuntu22.04_all.deb.2" "zabbix-release_6.4-1+ubuntu22.04_all.deb.3" "zabbix-release_6.4-1+ubuntu22.04_all.deb.4" "my-single-proc-squid" "my-double-proc-squid" "my-triple-proc-squid" "my-quad-proc-squid" "my-snapshot-squid" "fractal-node" ".bitcoin")
 
 # Сбор всех контейнеров с их статусами и временем создания
 ALL_CONTAINERS=$(docker ps -a --format '{{.Names}};{{.Status}};{{.CreatedAt}}')
 
-# Массивы для классификации контейнеров
-TO_KEEP=()           # Запущенные и необходимые контейнеры
-TO_REMOVE=()         # Контейнеры на удаление (запущенные или остановленные)
-UNKNOWN=()           # Неизвестные контейнеры
+# Сбор файлов и папок в текущей директории
+ITEMS=$(ls)
 
-# Функция для анализа контейнеров
+# Массивы для классификации Docker-контейнеров
+DOCKER_TO_KEEP=()
+DOCKER_TO_REMOVE=()
+DOCKER_UNKNOWN=()
+
+# Массивы для классификации файлов и папок
+FILES_TO_KEEP=()
+FILES_TO_REMOVE=()
+FILES_UNKNOWN=()
+
+# Функция для анализа Docker-контейнеров
 analyze_containers() {
   while IFS=';' read -r name status created_at; do
     container_info="$name (Статус: $status, Создан: $created_at)"
-
     if [[ " ${NECESSARY_CONTAINERS[@]} " =~ " $name " ]]; then
-      if [[ "$status" == Up* ]]; then
-        TO_KEEP+=("$container_info")
-      else
-      echo "///"
-      fi
+      DOCKER_TO_KEEP+=("$container_info")
     elif [[ " ${UNNECESSARY_CONTAINERS[@]} " =~ " $name " ]]; then
-      TO_REMOVE+=("$container_info")
+      DOCKER_TO_REMOVE+=("$container_info")
     else
-      UNKNOWN+=("$container_info")
+      DOCKER_UNKNOWN+=("$container_info")
     fi
   done <<< "$ALL_CONTAINERS"
 }
 
-# Анализируем контейнеры
+# Функция для анализа файлов и папок
+analyze_items() {
+  for item in $ITEMS; do
+    if [[ " ${NECESSARY_ITEMS[@]} " =~ " $item " ]]; then
+      FILES_TO_KEEP+=("$item")
+    elif [[ " ${UNNECESSARY_ITEMS[@]} " =~ " $item " ]]; then
+      FILES_TO_REMOVE+=("$item")
+    else
+      FILES_UNKNOWN+=("$item")
+    fi
+  done
+}
+
+# Выполнение анализа
 analyze_containers
+analyze_items
 
 # Запись отчета в файл в одну строку
 {
-   echo "$(date '+%Y-%m-%d'); $(IFS=','; echo "${FILES_TO_KEEP[*]}"); $(IFS=','; echo "${FILES_TO_REMOVE[*]}"); $(IFS=','; echo "${FILES_UNKNOWN[*]}"); $(IFS=','; echo "${DOCKER_TO_KEEP[*]}"); $(IFS=','; echo "${DOCKER_TO_REMOVE[*]}"); $(IFS=','; echo "${DOCKER_UNKNOWN[*]}")"
+  echo "$(date '+%Y-%m-%d'); $(IFS=','; echo "${FILES_TO_KEEP[*]}"); $(IFS=','; echo "${FILES_TO_REMOVE[*]}"); $(IFS=','; echo "${FILES_UNKNOWN[*]}"); $(IFS=','; echo "${DOCKER_TO_KEEP[*]}"); $(IFS=','; echo "${DOCKER_TO_REMOVE[*]}"); $(IFS=','; echo "${DOCKER_UNKNOWN[*]}")"
 } > "$REPORT_FILE"
+
 # Сообщение об успешной записи отчета
 echo "Отчет о Docker-контейнерах сохранен в $REPORT_FILE"
