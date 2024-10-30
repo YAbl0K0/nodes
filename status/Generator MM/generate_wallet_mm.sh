27a3f2a3940deed650b69441ecaac43c5d4a2a07
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-10-30 15:14:22 +0200

    Update generate_wallet_mm.sh

--- status/Generator MM/generate_wallet_mm.sh
+++ status/Generator MM/generate_wallet_mm.sh
@@ -1,34 +1,24 @@
 #!/bin/bash
 
-# Обробка помилок: якщо щось піде не так, скрипт завершиться
 set -e
-# Створюємо та переходимо в директорію для віртуального середовища
 mkdir -p evd_addr && cd evd_addr
 
-# Оновлюємо пакети та встановлюємо необхідні залежності
 apt update && apt install -y python3-venv python3-pip curl
 
-# Створюємо віртуальне середовище без pip
 python3 -m venv venv --without-pip
 
-# Активуємо віртуальне середовище
 source venv/bin/activate
 
-# Встановлюємо pip вручну
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py --quiet
-rm get-pip.py  # Видаляємо скрипт після використання
+rm get-pip.py
 
-# Встановлюємо бібліотеку для генерації гаманців
 pip install --quiet eth-account
 
-# Створюємо Python-скрипт для генерації гаманців
 wget https://raw.githubusercontent.com/YAbl0K0/nodes/master/status/Generator%20MM/wallets.py
-# Запускаємо Python-скрипт для генерації гаманців
+
 python wallets.py
 
-# Деактивуємо віртуальне середовище
 deactivate
 
-# Повертаємося в попередню директорію
 cd ..
