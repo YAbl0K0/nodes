b527617694152c9ff1e4a37f512a80229b5fa07b
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-11-04 15:36:08 +0200

    Create change_rpc.sh

---
+++ Change_RPC/change_rpc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Запрос нового значения для rpc_url / RPC_URL
+read -p "Введите новую rpc: " new_rpc
+
+# Файлы для изменения
+json_files=(
+    "$HOME/infernet-container-starter/deploy/config.json"
+    "$HOME/infernet-container-starter/projects/hello-world/container/config.json"
+)
+makefile="$HOME/infernet-container-starter/projects/hello-world/contracts/Makefile"
+
+# Замена rpc_url в JSON-файлах
+for file in "${json_files[@]}"; do
+    if [[ -f $file ]]; then
+        sed -i "s|\"rpc_url\": \".*\"|\"rpc_url\": \"$new_rpc\"|g" "$file"
+        echo "Значение rpc_url изменено в файле: $file"
+    else
+        echo "Файл не найден: $file"
+    fi
+done
+
+# Замена RPC_URL в Makefile
+if [[ -f $makefile ]]; then
+    sed -i "s|^RPC_URL :=.*|RPC_URL := $new_rpc|g" "$makefile"
+    echo "Значение RPC_URL изменено в файле: $makefile"
+else
+    echo "Файл не найден: $makefile"
+fi
+
+echo "Замена завершена."
