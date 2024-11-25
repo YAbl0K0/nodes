b07902a4c6ab56aca6c0b7a74ad550c967ee698e
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-11-25 20:34:25 +0200

    Create unstake.sh

---
+++ Shardeum/unstake.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Проверка, установлен ли tmux
+if ! command -v tmux &> /dev/null
+then
+    echo "tmux не установлен. Установите его с помощью команды: sudo apt install tmux"
+    exit 1
+fi
+
+# Запрос приватного ключа
+read -s -p "Введите приватный ключ от тестнет кошелька Shardeum: " PRIVATE_KEY
+echo ""
+
+# Название tmux-сессии
+SESSION_NAME="shardeum_unstake"
+
+# Запуск tmux-сессии
+tmux new-session -d -s $SESSION_NAME
+
+# Выполнение команд в tmux
+tmux send-keys -t $SESSION_NAME "cd ~/.shardeum && ./shell.sh" C-m
+tmux send-keys -t $SESSION_NAME "operator-cli stop" C-m
+tmux send-keys -t $SESSION_NAME "echo \"$PRIVATE_KEY\" | operator-cli unstake 10" C-m
+tmux send-keys -t $SESSION_NAME "sleep 30" C-m
+tmux send-keys -t $SESSION_NAME "exit" C-m
+
+# Информация о процессе
+echo "Вы можете проверить статус в любой момент командой: tmux attach -t $SESSION_NAME"
+echo "Анстейк выполнен"
