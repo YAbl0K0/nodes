1571d4e2fd5c8542e1280e66cdda1f1b38fa84e0
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-11-30 14:52:00 +0200

    Create autorizate_pipe.sh

---
+++ Pipe/autorizate_pipe.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "-----------------------------------------------------------------------------"
+echo "Авторизация и регистрация токена"
+echo "-----------------------------------------------------------------------------"
+
+pipe-tool login --node-registry-url="https://rpc.pipedev.network"
+pipe-tool generate-registration-token --node-registry-url="https://rpc.pipedev.network"
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable dcdnd
+sudo systemctl restart dcdnd
+
+pipe-tool generate-wallet --node-registry-url="https://rpc.pipedev.network" --key-path=$HOME/.permissionless/key.json
+pipe-tool link-wallet --node-registry-url="https://rpc.pipedev.network" --key-path=$HOME/.permissionless/key.json
+
+echo "-----------------------------------------------------------------------------"
+echo "Проверка логов"
+echo "journalctl -f -u dcdnd"
+echo "-----------------------------------------------------------------------------"
+echo "Wish lifechange case with DOUBLETOP"
+echo "-----------------------------------------------------------------------------"
