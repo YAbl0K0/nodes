c258b9cf7c4811d3f6b4e89802ed48e783693dcf
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-12-07 12:07:13 +0200

    Update analyz_0g.sh

--- status/analyz_0g.sh
+++ status/analyz_0g.sh
@@ -40,17 +40,18 @@ while IFS= read -r LINE; do
     TIME_DIFF=$((CURRENT_TIME - LAST_SEEN))
 
     # Преобразуем время последнего подключения в читаемый формат (относительно текущего времени)
-    LAST_SEEN_DATE=$(date -d "@$(( $(date +%s) - TIME_DIFF ))" +"%Y-%m-%d %H:%M:%S")
+    LAST_SEEN_TIME=$(date -d "@$(( $(date +%s) - TIME_DIFF ))" +"%s")
+    LAST_SEEN_DATE=$(date -d "@$(( $(date +%s) - TIME_DIFF ))" +"%Y-%m-%d")
 
     # Проверяем, не был ли IP активен последние 2 часа
     if (( TIME_DIFF > TWO_HOURS )); then
-        DISCONNECTED_IPS+=("$LAST_SEEN_DATE $IP")
+        DISCONNECTED_IPS+=("$LAST_SEEN_TIME $LAST_SEEN_DATE $IP")
     fi
 
     # Проверяем, является ли IP новым
     if ! grep -qw "$IP" "$KNOWN_IP_FILE"; then
         echo "$IP" >> "$KNOWN_IP_FILE"
-        NEW_IPS+=("$LAST_SEEN_DATE $IP")
+        NEW_IPS+=("$LAST_SEEN_TIME $LAST_SEEN_DATE $IP")
     fi
 done < "$LOG_FILE"
 
