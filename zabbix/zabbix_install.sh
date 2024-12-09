aac99dce18850bb32ebb08d9e5b36725638db870
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-12-09 20:14:33 +0200

    Update zabbix_install.sh

--- zabbix/zabbix_install.sh
+++ zabbix/zabbix_install.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+# Проверяем, установлена ли переменная HOST_NAME
+if [[ -z "$HOST_NAME" ]]; then
+    echo "Error: HOST_NAME is not set. Please set it before running the script."
+    echo "Example: HOST_NAME=1.2.101 ./run_zabbix.sh"
+    exit 1
+fi
+
 # Определяем версию Ubuntu
 UBUNTU_VERSION=$(lsb_release -rs | cut -d. -f1)
 
 # Проверяем версию и выполняем соответствующий скрипт
 if [[ "$UBUNTU_VERSION" == "20" ]]; then
     echo "Ubuntu 20.04 detected. Running install_zabbix.sh..."
-     . <(wget -qO- https://raw.githubusercontent.com/VadimRM7/zabix_ALL/main/install_zabbix.sh)
+    HOST_NAME=$HOST_NAME . <(wget -qO- https://raw.githubusercontent.com/VadimRM7/zabix_ALL/main/install_zabbix.sh)
 elif [[ "$UBUNTU_VERSION" == "22" ]]; then
     echo "Ubuntu 22.04 detected. Running install_zabbix22_04.sh..."
-     . <(wget -qO- https://github.com/VadimRM7/zabix_ALL/raw/refs/heads/main/install_zabbix22_04.sh)
+    HOST_NAME=$HOST_NAME . <(wget -qO- https://raw.githubusercontent.com/VadimRM7/zabix_ALL/main/install_zabbix22_04.sh)
 else
     echo "Unsupported Ubuntu version: $UBUNTU_VERSION"
     exit 1
