f8fbbac93b94958acc5d61be1d95743ab46c20c0
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-12-20 19:08:03 +0200

    Update 1status.sh

--- 0G/1status.sh
+++ 0G/1status.sh
@@ -5,4 +5,13 @@ PORT=8545
 
 # Проверяем активные подключения к порту
 echo "Список подключённых IP к порту $PORT:"
-ss -tn | grep ":$PORT" | awk '{print $5}' | cut -d':' -f1 | sort -u
+CONNECTED_IPS=$(ss -tan | awk -v port=":$PORT" '$5 ~ port {print $5}' | cut -d':' -f1 | sort -u)
+
+# Проверяем, есть ли активные подключения
+if [[ -z "$CONNECTED_IPS" ]]; then
+    echo "Нет подключённых IP по порту $PORT."
+    exit 0
+fi
+
+# Выводим список подключённых IP
+echo "$CONNECTED_IPS"
