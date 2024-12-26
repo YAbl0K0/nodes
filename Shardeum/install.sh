4c327d18d6d6d3a434a5a40c635c67e095392512
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-12-26 19:16:31 +0200

    Update install.sh

--- Shardeum/install.sh
+++ Shardeum/install.sh
@@ -22,12 +22,12 @@ while 1 {
         "By running this installer, you agree to allow the Shardeum team to collect this data. (Y/n)?:" {send "y\r"; exp_continue}
         "What base directory should the node use (default ~/shardeum):" {send "\r"; exp_continue}
         "Do you want to run the web based Dashboard? (Y/n):" {send "\r"; exp_continue}
-        "Set the password to access the Dashboard:" {send "111QwErTy*\r"; exp_continue}
         "Enter the port (1025-65536) to access the web based Dashboard (default 8080):" {send "8180\r"; exp_continue}
         "If you wish to set an explicit external IP, enter an IPv4 address (default=auto):" {send "\r"; exp_continue}
         "If you wish to set an explicit internal IP, enter an IPv4 address (default=auto):" {send "\r"; exp_continue}
         "This allows p2p communication between nodes. Enter the first port (1025-65536) for p2p communication (default 9001):" {send "\r"; exp_continue}
         "Enter the second port (1025-65536) for p2p communication (default 10001):" {send "\r"; exp_continue}
+        "Enter the password for accessing the Dashboard:" {send "111QwErTy*\r"; exp_continue}
         eof {break}
     }
 }
