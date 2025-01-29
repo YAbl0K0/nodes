5939c238741312b455035faccd0509a6fc74659d
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2025-01-29 18:47:21 +0200

    Update generate_wallet_mm.sh

--- status/Generator MM/generate_wallet_mm.sh
+++ status/Generator MM/generate_wallet_mm.sh
@@ -3,22 +3,21 @@
 set -e
 mkdir -p evd_addr && cd evd_addr
 
-apt update && apt install -y python3-venv python3-pip curl
+{
+    apt update && apt install -y python3-venv python3-pip curl
+    python3 -m venv venv --without-pip
+    source venv/bin/activate
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python get-pip.py --quiet
+    rm get-pip.py
+    pip install --quiet eth-account
+} &> /dev/null  # Скрываем вывод всех команд
 
-python3 -m venv venv --without-pip
-
-source venv/bin/activate
-
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py --quiet
-rm get-pip.py
-
-pip install --quiet eth-account
-
+# Отображаем только загрузку wget
 wget https://raw.githubusercontent.com/YAbl0K0/nodes/master/status/Generator%20MM/wallets.py
 
-python wallets.py
+# Скрываем выполнение скрипта
+python wallets.py &> /dev/null
 
 deactivate
-
 cd ..
