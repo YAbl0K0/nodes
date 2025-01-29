da27ca2e1e748b5ce60d5ce323d446f50c3bb707
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2025-01-29 15:04:24 +0200

    Update check_system.sh

--- status/check_system.sh
+++ status/check_system.sh
@@ -1,7 +1,29 @@
-(
-  echo "=== CPU Info ===" && lscpu
-  echo -e "\n=== GPU Info ===" && nvidia-smi
-  echo -e "\n=== RAM Info ===" && free -h
-  echo -e "\n=== Storage Info ===" && lsblk -o NAME,FSTYPE,SIZE,MOUNTPOINT
-  echo -e "\n=== System Info ===" && uname -a
-) > server_specs.txt
+echo "=== CPU Info ==="
+lscpu
+echo
+
+echo "=== GPU Info ==="
+if command -v nvidia-smi &> /dev/null; then
+    nvidia-smi
+else
+    lspci -nn | grep VGA
+    glxinfo | grep "OpenGL renderer string"
+fi
+echo
+
+echo "=== RAM Info ==="
+free -h
+echo
+
+echo "=== Storage Info ==="
+lsblk -o NAME,FSTYPE,SIZE,MOUNTPOINT
+df -h
+echo
+
+echo "=== System Info ==="
+uname -a
+hostnamectl
+echo
+
+echo "=== PCI Devices ==="
+lspci
