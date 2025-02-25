8b275a7e3558b332c297ad293f51a3c25242082a
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2025-02-25 21:42:40 +0200

    Update find_status.sh

--- status/find_status.sh
+++ status/find_status.sh
@@ -1,41 +1,30 @@
 #!/bin/bash
 
-# Параметры
+# Определение цветов для вывода
+RED='\033[0;31m'    # Красный (Удалить)
+GREEN='\033[0;32m'  # Зеленый (Оставить)
+BLUE='\033[0;34m'   # Синий (Неизвестное)
+NC='\033[0m'        # Сброс цвета
+
+# Определение необходимых и ненужных элементов
 NECESSARY_ITEMS=("0g_backup" "0g-chain" "0g-storage-client" "0g-storage-node" "3proxy-0.9.3" "all-in-one-v2" "allora-chain" "allora-huggingface-walkthrough" "allora-worker-x-reputer" "basic-coin-prediction-node" "bevm" "btop.tbz" "data" "elixir" "go" "heminetwork" "infernet-container-starter" "installer.sh" "install_zabbix.sh" "nillion" "sentry-node-cli-linux" "star_labs" "start.sh" "subspace_docker" "update.txt" "vmagent-prod" "vmalert-prod" "vmauth-prod" "vmbackup-prod" "vmctl-prod" "vmrestore-prod" "zabbix-release_6.4-1+ubuntu22.04_all.deb" ".0gchain" ".config" ".profile" ".docker")
 UNNECESSARY_ITEMS=("0gchain_snapshot.lz4" "0gchain_snapshot.lz4.aria2" "0.9.3.tar.gz" "0.9.3.tar.gz.1" "BeraMachine" "dusk_global_height.json" "gear" "gear_key" "masa-oracle-go-testnet" "my-cryptopunks-squid" "sentry-node-cli-linux.zip" "storage_0gchain_snapshot.lz4" "uniform-load-squid" "zabbix-release_6.4-1+ubuntu22.04_all.deb.1" "zabbix-release_6.4-1+ubuntu22.04_all.deb.2" "zabbix-release_6.4-1+ubuntu22.04_all.deb.3" "zabbix-release_6.4-1+ubuntu22.04_all.deb.4" "my-single-proc-squid" "my-double-proc-squid" "my-triple-proc-squid" "my-quad-proc-squid" "my-snapshot-squid" "uniform-load-squid" "fractal-node" ".bitcoin")
 
-# Автоматическое создание имени файла отчета с текущей датой и временем
-REPORT_FILENAME="report_$(date '+%Y-%m-%d_%H-%M-%S').txt"
-REPORT_FILE="./$REPORT_FILENAME"
-
-# Сбор всех файлов и папок в текущей директории
+# Получение списка файлов и папок в текущей директории
 ITEMS=$(ls)
 
-# Массивы для классификации элементов
-TO_KEEP=()
-TO_REMOVE=()
-UNKNOWN=()
-
-# Функция для анализа элементов
+# Функция анализа элементов
 analyze_items() {
   for item in $ITEMS; do
     if [[ " ${NECESSARY_ITEMS[@]} " =~ " $item " ]]; then
-      TO_KEEP+=("$item")
+      echo -e "${GREEN}✔ Оставить: $item${NC}"
     elif [[ " ${UNNECESSARY_ITEMS[@]} " =~ " $item " ]]; then
-      TO_REMOVE+=("$item")
+      echo -e "${RED}✖ Удалить: $item${NC}"
     else
-      UNKNOWN+=("$item")
+      echo -e "${BLUE}❓ Неизвестное: $item${NC}"
     fi
   done
 }
 
-# Анализируем файлы и папки
+# Запуск анализа
 analyze_items
-
-# Запись отчета в файл в одну строку
-{
-  echo "$(date '+%Y-%m-%d'); TO_KEEP: $(IFS=','; echo "${TO_KEEP[*]}"), TO_REMOVE: $(IFS=','; echo "${TO_REMOVE[*]}"), UNKNOWN: $(IFS=','; echo "${UNKNOWN[*]}")"
-} > "$REPORT_FILE"
-
-# Сообщение об успешной записи отчета
-echo "Отчет сохранен в $REPORT_FILE"
