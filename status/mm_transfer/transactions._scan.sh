7e28073f8ce26ecb7f8c7ac26a536f86db275b19
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2025-02-25 11:03:52 +0200

    Update transactions._scan.sh

--- status/mm_transfer/transactions._scan.sh
+++ status/mm_transfer/transactions._scan.sh
@@ -40,7 +40,7 @@ while read -r WALLET_ADDRESS; do
     if [[ -n "$WALLET_ADDRESS" ]]; then
         get_last_transaction_date "BNB (BSC)" "https://api.bscscan.com/api" "$BSC_API_KEY" "$WALLET_ADDRESS"
         get_last_transaction_date "Arbitrum" "https://api.arbiscan.io/api" "$ARB_API_KEY" "$WALLET_ADDRESS"
-        get_last_transaction_date "opBNB" "https://api.opbnbscan.com/api" "$OPBNB_API_KEY" "$WALLET_ADDRESS"
+        get_last_transaction_date "opBNB" "https://api-opbnb.bscscan.com/api" "$OPBNB_API_KEY" "$WALLET_ADDRESS"
         get_last_transaction_date "Mantle" "https://api.mantlescan.xyz/api" "$MNT_API_KEY" "$WALLET_ADDRESS"
     fi
 done < "$WALLETS_FILE"
