c5f7ee211a3da634ea73ac8b90cd8b47475e097c
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2025-03-01 21:45:49 +0200

    Update full_status.sh

--- status/full_status.sh
+++ status/full_status.sh
@@ -6,14 +6,19 @@ BLUE='\033[0;34m'   # Синий (Неизвестное)
 NC='\033[0m'        # Сброс цвета
 
 # Параметры для Docker-контейнеров
-NECESSARY_CONTAINERS=("elixir" "ipfs_node" "orchestrator" "shardeum-validator" "deploy-fluentbit-1" "infernet-node" "hello-world"  "shardeum-dashboard" "updater" "mongodb" "docker-watchtower-1")
+NECESSARY_CONTAINERS=("elixir" "ipfs_node" "orchestrator" "shardeum-validator" "deploy-fluentbit-1" "infernet-node" "hello-world" "deploy-redis-1" "shardeum-dashboard" "updater" "mongodb" "docker-watchtower-1")
 
 # Массивы для контейнеров с раздельными именами и ссылками
 UNNECESSARY_CONTAINERS_NAMES=("subspace_docker-node-1" "allora-worker" "boolnetwork" "subspace_docker-farmer-1" "kroma-validator" "kroma-node" "kroma-geth" "run_nillion" "source-02" "source-03" "source-01" "bevm-node")
 UNNECESSARY_CONTAINERS_LINKS=("https://example.com/subspace_docker-node-1" "https://example.com/allora-worker" "https://example.com/boolnetwork" "https://example.com/subspace_docker-farmer-1" "https://example.com/kroma-validator" "https://example.com/kroma-node" "https://example.com/kroma-geth" "https://example.com/run_nillion" "https://example.com/source-02" "https://example.com/source-03" "https://example.com/source-01" "https://example.com/bevm-node")
 
 # Массивы для файлов и папок
-NECESSARY_ITEMS=("0g_backup" "0g-chain" "0g-storage-client" ".bashrc" ".sonaric" "0g-storage-node" ".nesa" "multipleforlinux" "multiple-node" "3proxy-0.9.3" "foundry" "data" "elixir" "go" "infernet-container-starter" "sentry-node-cli-linux" "start.sh" "update.txt" "vmagent-prod" "vmalert-prod" "vmauth-prod" "vmbackup-prod" "vmctl-prod" "vmrestore-prod" ".0gchain" ".config" ".profile" ".docker")
+NECESSARY_ITEMS=(
+  "3proxy-0.9.3" ".bashrc" ".docker" "foundry" "multipleforlinux" "pipe_backup" "snap" ".wget-hsts"
+  ".bash_history" ".cache" "elixir" "infernet-container-starter" "multiple-node" ".profile" ".ssh"
+  "0.9.3.tar.gz" ".bash_profile" ".cloud-locale-test.skip" ".foundry" ".local" "opt" "shardeum" ".svm"
+)
+
 UNNECESSARY_ITEMS_NAMES=("0gchain_snapshot.lz4" "0gchain_snapshot.lz4.aria2" "rusk" "namada" "masa-oracle-go-testnet" "light_0gchain_snapshot.lz4" "BeraMachine" "gear" "bevm" "allora-worker-x-reputer" "nillion" "my-cryptopunks-squid" "storage_0gchain_snapshot.lz4" "fractal-node" ".bitcoin")
 UNNECESSARY_ITEMS_LINKS=("https://example.com/0gchain_snapshot.lz4" "https://example.com/0gchain_snapshot.lz4.aria2" "https://example.com/rusk" "https://example.com/namada" "https://example.com/masa-oracle-go-testnet" "https://example.com/light_0gchain_snapshot.lz4" "https://example.com/BeraMachine" "https://example.com/gear" "https://example.com/bevm" "https://example.com/allora-worker-x-reputer" "https://example.com/nillion" "https://example.com/my-cryptopunks-squid" "https://example.com/storage_0gchain_snapshot.lz4" "https://example.com/fractal-node" "https://example.com/.bitcoin")
 
@@ -21,11 +26,18 @@ UNNECESSARY_ITEMS_LINKS=("https://example.com/0gchain_snapshot.lz4" "https://exa
 ALL_CONTAINERS=($(docker ps -a --format '{{.Names}}'))
 
 # Сбор файлов и папок в текущей директории
-ITEMS=($(find . -mindepth 1 -maxdepth 1 -printf "%f\n"))
+ITEMS=($(find . -mindepth 1 -maxdepth 1 -exec basename {} \;))
 
 # Отладочный вывод
-echo -e "\nВсе контейнеры: ${ALL_CONTAINERS[*]}"
-echo -e "\nВсе файлы и папки: ${ITEMS[*]}"
+echo -e "\nВсе контейнеры:"
+for container in "${ALL_CONTAINERS[@]}"; do
+  echo " - $container"
+done
+
+echo -e "\nВсе файлы и папки:"
+for item in "${ITEMS[@]}"; do
+  echo " - $item"
+done
 
 # Функция анализа Docker-контейнеров
 analyze_containers() {
@@ -34,7 +46,7 @@ analyze_containers() {
     name="${UNNECESSARY_CONTAINERS_NAMES[$i]}"
     link="${UNNECESSARY_CONTAINERS_LINKS[$i]}"
     for container in "${ALL_CONTAINERS[@]}"; do
-      if [[ "$container" == "$name" ]]; then
+      if [[ "$container" == "$name" ]] && [[ ! " ${NECESSARY_CONTAINERS[*]} " =~ " $name " ]]; then
         echo -e "${RED}✖ $name ($link) ()${NC}"
       fi
     done
