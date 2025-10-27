0fd41173109bf1daa080a5adfdde4ea18038c5e7
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2025-10-27 14:16:58 +0200

    Update start_transf.sh

--- status/mm_transfer/start_transf.sh
+++ status/mm_transfer/start_transf.sh
@@ -1,17 +1,5 @@
-#!/bin/bash
-
-read -p "Введите адрес кошелька получателя (0x...): " RECIPIENT
-
-# Проверяем, что адрес начинается с 0x
-if [[ ! $RECIPIENT =~ ^0x[0-9a-fA-F]{40}$ ]]; then
-  echo "❌ Ошибка: некорректный адрес. Должен начинаться с 0x и содержать 40 символов."
-  exit 1
-fi
-
-# Запуск команды
-echo "Выполняется перевод на адрес: $RECIPIENT ..."
 python3 transfer_0g.py \
-  --recipient "$RECIPIENT" \
+  --recipient 0xВАШ_АДРЕС_X \
   --mode all \
   --leave-native 0.01 \
   --mnemonics mnemonics.txt \
